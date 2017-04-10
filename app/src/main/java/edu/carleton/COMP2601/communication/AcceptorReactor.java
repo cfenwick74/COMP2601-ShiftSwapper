0a3d9dfa07a508dd8dcb19b6677dad08a8b698e7
Pierre Seguin <Pierre Seguin>
2017-04-10 16:56:39 -0400

    Added acceptorReactor class

---
+++ app/src/main/java/edu/carleton/COMP2601/communication/AcceptorReactor.java
@@ -0,0 +1,72 @@
+package edu.carleton.COMP2601.communication;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.io.Serializable;
+import java.net.Socket;
+import java.util.HashMap;
+
+import edu.carleton.COMP2601.MainActivity;
+
+/**
+ * Created by Pierre on 4/10/2017.
+ */
+
+public class AcceptorReactor {
+    protected Reactor r;
+    Socket s;
+    JSONEventSource e;
+    ThreadWithReactor twr;
+    static AcceptorReactor instance = new AcceptorReactor();
+    String addr;
+    int port;
+    public AcceptorReactor getInstance(){return instance;}
+
+    private AcceptorReactor(){
+        r = new Reactor();
+    }
+
+    void init(int port, String addr){
+        this.port = port;
+        this.addr = addr;
+    }
+
+    void register(String type, EventHandler e) {
+        twr.register(type, e);
+    }
+
+    void start(final Event event){
+        Thread connectThread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+
+                try {
+                    s = new Socket(addr, port);
+                    e = new JSONEventSource(s);
+                    twr = new ThreadWithReactor(e, r);
+                    twr.start(event);
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        connectThread.start();
+    }
+
+    /**
+     * Sends an Event to the server
+     * @param e the Event to send
+     */
+    void put(Event e) {
+        if (twr != null) {
+            try {
+                ((JSONEventSource) twr.getEventSource()).putEvent(e);
+            } catch (IOException e1) {
+                e1.printStackTrace();
+            }
+        }
+    }
+}
