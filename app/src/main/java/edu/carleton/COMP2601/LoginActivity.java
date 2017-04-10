d96a707de34740a5c4813136ea4d25abb0ae057f
Carolyn Fenwick <carolyn@irondatabase.com>
2017-04-10 16:54:31 -0400

    adding login button functionality

--- app/src/main/java/edu/carleton/COMP2601/LoginActivity.java
+++ app/src/main/java/edu/carleton/COMP2601/LoginActivity.java
@@ -6,18 +6,38 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.io.Serializable;
+import java.lang.reflect.Field;
+import java.net.Socket;
+import java.util.HashMap;
+
+import edu.carleton.COMP2601.communication.Event;
+import edu.carleton.COMP2601.communication.EventHandler;
+import edu.carleton.COMP2601.communication.Fields;
+import edu.carleton.COMP2601.communication.JSONEvent;
 import edu.carleton.COMP2601.communication.JSONEventSource;
+import edu.carleton.COMP2601.communication.Reactor;
 import edu.carleton.COMP2601.communication.ThreadWithReactor;
 
 public class LoginActivity extends AppCompatActivity {
 
 	Button loginBtn;
 	EditText userName, password, host, port_field;
-	private String empId, pwd, ipaddr, port;
+	private String empId, pwd, ipaddr;
+	private int port;
+	ProgressBar spinner;
 	static LoginActivity instance;
 	JSONEventSource e;
 	ThreadWithReactor twr;
+	Socket s;
+	Reactor r;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +45,8 @@ public class LoginActivity extends AppCompatActivity {
 		setContentView(R.layout.activity_login);
 		instance = this;
 
+		spinner = (ProgressBar) findViewById(R.id.progressBar);
+		spinner.setVisibility(View.GONE);
 		loginBtn = (Button)findViewById(R.id.login_btn);
 		userName = (EditText) findViewById(R.id.login_text);
 		password = (EditText) findViewById(R.id.password_text);
@@ -35,11 +57,104 @@ public class LoginActivity extends AppCompatActivity {
 
 	public void login(View view) {
 		ipaddr = host.getText().toString();
+		empId = userName.getText().toString();
+		pwd = password.getText().toString();
+		port = Integer.parseInt(port_field.getText().toString());
 
+		Thread connectThread = new Thread(new Runnable() {
+			@Override
+			public void run() {
+				try {
+					s = new Socket(ipaddr, port);
+					e = new JSONEventSource(s);
+					init();
+					twr = new ThreadWithReactor(e, r);
+
+					HashMap m = new HashMap();
+					m.put(Fields.ID, empId);
+					m.put(Fields.PASSWORD, pwd);
+
+					spinner.setIndeterminate(true);
+					JSONObject jo = new JSONObject();
+					jo.put(Fields.TYPE, Fields.CONNECT_REQUEST);
+					jo.put(Fields.SOURCE, empId);
+					jo.put(Fields.DEST, "");
+
+					twr.start(new JSONEvent(jo, null, m));
+
+				} catch (IOException | JSONException e) {
+					e.printStackTrace();
+				}
+			}
+		});
+		connectThread.start();
 
 		//TODO: message with map JSON event
 		// Fields.id with login -- source
-		Intent intent = new Intent(LoginActivity.this, AdminActivity.class);
-		startActivity(intent);
+
 	}
+
+
+	@Override
+	protected void onDestroy() {
+		super.onDestroy();
+
+		try {
+			JSONObject jo = new JSONObject();
+			jo.put("Type", Fields.DISCONNECT_REQUEST);
+			jo.put("Source", empId);
+			jo.put("Dest", "");
+			jo.put("Message", "");
+			sendToServer(new JSONEvent(jo, null, new HashMap<String, Serializable>()));
+		} catch (JSONException e1) {
+			e1.printStackTrace();
+		}
+	}
+
+	/**
+	 * Sends an Event to the server
+	 * @param e the Event to send
+	 */
+	void sendToServer(Event e) {
+		if (twr != null) {
+			try {
+				((JSONEventSource) twr.getEventSource()).putEvent(e);
+			} catch (IOException e1) {
+				e1.printStackTrace();
+			}
+		}
+	}
+
+	/**
+	 * Initialize reactor and register connection handler
+	 */
+	void init() {
+		r = new Reactor();
+		LoginActivity.ConnectResponseHandler connectResponseHandler = new ConnectResponseHandler();
+		r.register(Fields.CONNECTED_RESPONSE, connectResponseHandler);
+	}
+
+
+	private class ConnectResponseHandler implements EventHandler {
+		@Override
+		public void handleEvent(Event event) {
+			runOnUiThread(new Runnable() {
+				@Override
+				public void run() {
+					Toast.makeText(LoginActivity.this, "Connected!", Toast.LENGTH_SHORT).show();
+					spinner.setVisibility(View.GONE);
+				}
+			});
+
+			Intent intent = new Intent(LoginActivity.this, UserListActivity.class);
+//			intent.putExtra("name", name);
+			startActivity(intent);
+		}
+	}
+
+
 }
+
+
+//	Intent intent = new Intent(LoginActivity.this, AdminActivity.class);
+//	startActivity(intent);
